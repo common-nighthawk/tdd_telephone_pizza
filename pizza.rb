a51dad3d7c419233f879641e1671ee04f7e08a88
Daniel Deutsch <danielallendeutsch@gmail.com>
2014-06-03 11:16:34 -0500

    more initialize tests added

--- pizza.rb
+++ pizza.rb
@@ -1,2 +1,13 @@
 class Pizza
+  attr_reader :name, :description, :time_baked
+
+  def initialize(args)
+    @description = args[:description]
+    @time_baked = args[:time_baked]
+  end
+
+  def name
+    "Papa John's Deluxe"
+  end
+
 end
--- pizza_spec.rb
+++ pizza_spec.rb
@@ -5,22 +5,45 @@ describe Pizza do
     let (:pizza) {Pizza.new(
       name: "papa john's deluxe",
       description: "Sammy's favorite pizza",
-      time_baked: 25
-      )
+      time_baked: 25)
+    }
+    let (:pizza2) {Pizza.new(
+      name: "lou manaltis deluxe",
+      description: "Mo's favorite pizza",
+      time_baked: 45)
     }
 
     it "returns name" do
-      expect pizza.name.to eq "papa john's deluxe"
+      expect(pizza.name).to eq "papa john's deluxe"
+    end
+
+    it "returns a different name" do
+      expect(pizza2.name).to eq "lou manaltis deluxe"
     end
 
     it "returns description" do
-      expect pizza.description.to eq "Sammy's favorite pizza"
+      expect(pizza.description).to eq "Sammy's favorite pizza"
+    end
+
+    it "returns a different description" do
+      expect(pizza2.description).to eq "Mo's favorite pizza"
     end
 
     it "returns description" do
-      expect pizza.time_baked.to eq 25
+      expect(pizza.time_baked).to eq 25
     end
+
+    it "returns description" do
+      expect(pizza2.time_baked).to eq 45
+    end
+
 
 
   end
+
+  # it "must be initialized with a name thats a string"
+
+  # end
+
+
 end
